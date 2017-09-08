b741fc49a7be8f3b6075ea1070cc980a9a081767
a-nizam <nyzam7@gmail.com>
2017-09-08 15:43:16 +0300

    Built

--- src/sample/SampleController.java
+++ src/sample/SampleController.java
@@ -1,12 +1,18 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 
 public class SampleController {
 
+    @FXML
+    public TextField tfName;
+
+    @FXML
     public void send(ActionEvent actionEvent) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Привет!!!");
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, tfName.getText());
         alert.showAndWait();
         System.out.print("Привет!\r\n");
     }
